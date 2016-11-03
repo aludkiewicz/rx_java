66d3845a57d4cf2e3821e35e6a92617681ee35b7
Alexander Ludkiewicz <aludgm@gmail.com>
2016-11-03 13:37:35 +0100

    Refactoring and added a simple class with static utility methods to simulate e.g. network calls or streaming data

---
+++ src/main/java/com/hicollege/rxjava/EventProducer.java
@@ -0,0 +1,28 @@
+package com.hicollege.rxjava;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
+public class EventProducer {
+
+	public static Stream<String> produceStrings() {
+		Stream<String> stream = Stream.generate(() -> generateStringsWithDelay(0));
+		return stream;
+	}
+
+	public static Stream<String> produceStringsWithDelay(int delayInMillis) {
+		Stream<String> stream = Stream.generate(() -> generateStringsWithDelay(delayInMillis));
+		return stream;
+	}
+
+	private static String generateStringsWithDelay(int millis) {
+		if(millis > 0) {
+			try {
+				Thread.sleep(millis);
+			} catch (InterruptedException e) {}
+		}
+		return Integer.toString(ThreadLocalRandom.current().nextInt(0, 100 + 1));
+	}
+
+
+}
--- src/main/java/com/hicollege/rxjava/RxJavaPlayground.java
+++ src/main/java/com/hicollege/rxjava/RxJavaPlayground.java
@@ -1,7 +1,11 @@
 package com.hicollege.rxjava;
 
+import java.util.stream.Stream;
+
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 public class RxJavaPlayground {
 
@@ -35,6 +39,19 @@ public class RxJavaPlayground {
 		 */
 		myObservable = Observable.just("Hello World!");
 		myObservable.subscribe(s -> System.out.println(s + " v2"));
+
+		/*
+		 * Creating an observable of an infinite stream
+		 */
+		Observable<Stream<String>> myObservable2 = Observable.create(t -> {
+			t.onNext(EventProducer.produceStringsWithDelay(100));
+			t.onCompleted();
+		});
+
+		Action1<? super Stream<String>> onNextAction = strm -> strm.limit(20).forEach(s -> System.out.println(s));
+		Action1<Throwable> onErrorAction = ex -> System.out.println(ex);
+		Action0 onCompletedAction = () -> System.out.println("Derp");
+		myObservable2.subscribe(onNextAction, onErrorAction, onCompletedAction);
 
 
 
